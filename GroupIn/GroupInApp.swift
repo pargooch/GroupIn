7732bd8d3d0d24d3a91fba7cc23ad9f689a9deed
Pargooch🐰 <novindokhtelmi@aol.com>
2026-05-05 14:12:43 +0200

    Initial Commit

---
+++ GroupIn/GroupInApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupInApp.swift
+//  GroupIn
+//
+//  Created by Novin dokht Elmi on 05/05/26.
+//
+
+import SwiftUI
+
+@main
+struct GroupInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
